3524877b2b064cded9ef28cf3b97371021d5a2b4
Joshua Flack <joshu.alexander@gmail.com>
2017-06-08 15:25:57 -0400

    finished

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 // Add your JS here.
+
+var todoInput = document.querySelector('.new-todo');
+
+// Select the ordered list element with the id 'todoList' and store in a variable `todoList`.
+
+var todoList = document.querySelector('.todo-list');
+
+
+
+// Add a keypress event listener to `todoInput`.
+
+todoInput.addEventListener('keypress', function(event)
+{if (event.keyCode === 13){
+  var todoContent = event.target.value;
+  event.target.value = "";
+  var newTodoLi = document.createElement('li');
+  newTodoLi.textContent = todoContent;
+  todoList.appendChild(newTodoLi);
+  newTodoLi.innerHTML = "<li>" + "<class = 'view'>" + "<label>" + newTodoLi.textContent;
+}
+return event;
+}
+);
